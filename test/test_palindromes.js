e493e8469bb1a523d282e72c4f61bcafb90c8e44
notadeejay <amy@lighthouselabs.ca>
2019-07-04 14:36:12 -0400

    var => const

--- test/test_palindromes.js
+++ test/test_palindromes.js
@@ -1,25 +1,25 @@
-var assert = require("chai").assert;
-var isPalindrome = require("../lib/palindromes");
+const assert = require("chai").assert;
+const isPalindrome = require("../lib/palindromes");
 
 describe("Palindromes", function() {
   it("should return true if a word is a palindrome", function() {
-    var word = "abba";
-    var result = isPalindrome(word);
+    const word = "abba";
+    const result = isPalindrome(word);
     assert.isTrue(result);
   });
   
   it("should return false if a word is NOT a palindrome", function() {
-    var word = "not";
+    const word = "not";
     assert.isFalse(isPalindrome(word));
   });
   
   it("should return true if a phrase is a palindrome", function() {
-    var phrase = "a man a plan a canal panama";
+    const phrase = "a man a plan a canal panama";
     assert.isTrue(isPalindrome(phrase));
   });
   
   it("should return false if a phrase is NOT a palindrome", function() {
-    var phrase = "this is not a palindrome"
+    const phrase = "this is not a palindrome"
     assert.isFalse(isPalindrome(phrase));
   });
 });
